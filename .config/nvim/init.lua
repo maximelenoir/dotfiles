bde337fd3bd55bfc1b6c931f592f8d3dae1be9a2
Maxime Lenoir <lenoir.maxime0@gmail.com>
2023-03-02 20:40:10 +0100

    Move from init.vim to init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.wo.number = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+require('plugins')
+require('lspconfig').rust_analyzer.setup({})
--- .config/nvim/init.vim
+++
@@ -1,5 +0,0 @@
-set tabstop=4
-set shiftwidth=4
-set expandtab
-lua require('plugins')
-lua require('lspconfig').rust_analyzer.setup({})
