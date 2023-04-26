0e8c6e429bf1f46eb61aaefe541f1bcee74f5520
Maxime Lenoir <lenoir.maxime0@gmail.com>
2023-04-26 19:02:56 +0200

    Auto selection in cmp menu

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -35,7 +35,8 @@ cmp.setup({
 		{ name = 'vsnip' },
 	}, {
 		{ name = 'buffer' },
-	})
+	}),
+    completion = { completeopt = 'menu,menuone,noinsert' },
 })
 
 local cmp_caps = require('cmp_nvim_lsp').default_capabilities()
